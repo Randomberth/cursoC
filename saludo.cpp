b3e3bdfcbc9f73740f2b20404d7336f2d2e10ef7
Aliberth <aliberth@kaliRandomberth.org>
2025-09-26 12:48:16 -0400

    test2 corpo

---
+++ saludo.cpp
@@ -0,0 +1,12 @@
+// saludo.c
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hola mundo!!!. \n");
+    printf("\n");
+    printf("somos unas bestias de la programacion... a por toda!!!! \n");
+    return 0;
+}
