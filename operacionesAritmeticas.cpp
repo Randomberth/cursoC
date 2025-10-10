8929c2b73b2ecd522d0f8f3571f622afa6173e29
Aliberth <aliberth@kaliRandomberth.org>
2025-10-10 08:43:37 -0400

    test3 corpo

---
+++ operacionesAritmeticas.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main ()
+{
+ int dato1, dato2, resp;
+
+    dato1 = 13;
+    dato2 = 15;
+    
+    // Suma
+    resp = dato 1 + dato2;
+    printf("%d - %d = %d,  dato1, dato2, resp");
+
+    // Suma
+    resp = dato 1 + dato2;
+    printf("%d - %d = %d,  dato1, dato2, resp");
+
+    // Suma
+    resp = dato 1 + dato2;
+    printf("%d - %d = %d,  dato1, dato2, resp");
+
+    // Cociente
+    resp = dato 1 + dato2;
+    printf("%d - %d = %d,  dato1, dato2, resp");
+
+
+}
