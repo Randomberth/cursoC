813ee235f4284a75128d8fa8cfad9ed8fb5710a2
Alibert <aliberth33@gmail.com>
2025-10-11 12:57:17 -0400

    test 7 house calculadora

--- calculadoraT.cpp
+++ calculadoraT.cpp
@@ -4,7 +4,8 @@ int main()
 {
 
     short unsigned op = 0;
-    float valor1 = 0, valor2 = 0, result = 0;
+    int valor1 = 0, valor2 = 0, modulo = 0;
+    float result = 0;
 
     printf("\n");
     printf("    Bienvenido a calculadora");
@@ -29,38 +30,62 @@ int main()
     case 1:
         printf("    Operación SUMA \n \n");
         printf("    Ingrese primer valor : ");
-        scanf("%f", &valor1);
+        scanf("%d", &valor1);
         printf("    Ingrese segundo valor : ");
-        scanf("%f", &valor2);
+        scanf("%d", &valor2);
         printf("\n \n");
-        printf("    valor1 = %.2f, valor2 = %.2f \n \n", valor1, valor2);
+        printf("    valor1 = %d, valor2 = %d \n \n", valor1, valor2);
+        printf("\n \n");
+
+        result = valor1 + valor2;
+        printf("El resultado de la suma es : %.2f", result);
+        printf("\n \n");
         break;
     case 2:
         printf("    Operación RESTA \n \n");
         printf("    Ingrese primer valor : ");
-        scanf("%f", &valor1);
+        scanf("%d", &valor1);
         printf("    Ingrese segundo valor : ");
-        scanf("%f", &valor2);
+        scanf("%d", &valor2);
         printf("\n \n");
-        printf("    valor1 = %.2f, valor2 = %.2f \n \n", valor1, valor2);
+        printf("    valor1 = %d, valor2 = %d \n \n", valor1, valor2);
+        printf("\n \n");
+
+        result = valor1 - valor2;
+        printf("El resultado de la resta es : %.2f", result);
+        printf("\n \n");
         break;
     case 3:
         printf("    Operación PRODUCTO \n \n");
         printf("    Ingrese primer valor : ");
-        scanf("%f", &valor1);
+        scanf("%d", &valor1);
         printf("    Ingrese segundo valor : ");
-        scanf("%f", &valor2);
+        scanf("%d", &valor2);
         printf("\n \n");
-        printf("    valor1 = %.2f, valor2 = %.2f \n \n", valor1, valor2);
+        printf("    valor1 = %d, valor2 = %d \n \n", valor1, valor2);
+        printf("\n \n");
+
+        result = valor1 * valor2;
+        printf("El resultado de la multiplicación es : %.2f", result);
+        printf("\n \n");
+
         break;
     case 4:
         printf("    Operación DIVISIÓN \n \n");
         printf("    Ingrese primer valor : ");
-        scanf("%f", &valor1);
+        scanf("%d", &valor1);
         printf("    Ingrese segundo valor : ");
-        scanf("%f", &valor2);
+        scanf("%d", &valor2);
         printf("\n \n");
-        printf("    valor1 = %.2f, valor2 = %.2f \n \n", valor1, valor2);
+        printf("    valor1 = %d, valor2 = %d \n \n", valor1, valor2);
+        printf("\n \n");
+
+        result = valor1 / valor2;
+        modulo = valor1 % valor2;
+
+        printf("    El resultado de la división es : %.2f \n", result);
+        printf("    y su módulo es : %d", modulo);
+        printf("\n \n");
         break;
 
     default:
