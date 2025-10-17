976b5126ed62a48a24dc2ea883f1829fe942d08b
Aliberth <aliberth@kaliRandomberth.org>
2025-10-17 11:46:56 -0400

    test 16 corpo test casting

---
+++ casting.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int test_int = 25, a = 0, b = 0;
+    float test_float = 253.2;
+    string test_string = "25";
+
+    cout << " test casteo : " << ((float)test_int / 2);
+    return (0);
+}
